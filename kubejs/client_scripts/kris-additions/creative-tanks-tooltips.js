3bb3b8bf0941d760ef4e82d066288e0bb3cfe99f
kris j <kris.jurkans@gmail.com>
2025-05-06 02:53:48 +0100

    add example tooltip

---
+++ kubejs/client_scripts/kris-additions/creative-tanks-tooltips.js
@@ -0,0 +1,6 @@
+// https://kubejs.com/wiki/tutorials/tooltips
+// https://minecraft.fandom.com/wiki/Formatting_codes?so=search#Use_in_server.properties_and_pack.mcmeta
+
+ItemEvents.tooltip(event => {
+    event.add("kubejs:block_of_antimatter", ["\u00A76Kris' Additions"])
+})
