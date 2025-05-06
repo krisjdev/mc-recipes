3232570c5f516daa4b9a1818c8748009b742d8c4
kris j <kris.jurkans@gmail.com>
2025-05-06 19:33:41 +0100

    generate blocks of gas for creative tank recipes

--- kubejs/server_scripts/kris-additions/creative-tanks.js
+++ kubejs/server_scripts/kris-additions/creative-tanks.js
@@ -17,23 +17,73 @@ ServerEvents.recipes(event => {
         }
     ).id("krisadditions:mekanism/creative_chemical_tank")
 
-    // block of antimatter
-    event.custom({
-        type: "mekanism:nucleosynthesizing",
-        duration: 4000,
-        gasInput: {
-            amount: 1000,
-            gas: "mekanism:antimatter"
-        },
-        itemInput: {
-            ingredient: {
-                item: "allthecompressed:cobblestone_9x"
+    
+    // blocks of gas
+    // used to make the creative chemical tank variants in energiser recipe
+
+    var block_dict = {
+        // valid keys are:
+        //      input, gas_input, gas_amount, duration
+        //
+        // input defaults to "allthecompressed:cobblestone_9x"
+        // duration defaults to 400
+        // gas_amount defaults to 10,000
+        // gas_input is required and has no default value
+
+        // TODO: probably could make the gas input string using the key to reduce repetition
+        antimatter: {gas_input: "mekanism:antimatter", gas_amount: 1000, duration: 400},
+        brine: {gas_input: "mekanism:brine"},
+        chlorine: {gas_input: "mekanism:chlorine"},
+        ethene: {gas_input: "mekanism:ethene"},
+        fissile_fuel: {gas_input: "mekanism:fissile_fuel"},
+        hydrofluoric_acid: {gas_input: "mekanism:hydrofluoric_acid"},
+        hydrogen: {gas_input: "mekanism:hydrogen"},
+        hydrogen_chloride: {gas_input: "mekanism:hydrogen_chloride"},
+        lithium: {gas_input: "mekanism:lithium"},
+        nuclear_waste: {gas_input: "mekanism:nuclear_waste"},
+        osmium: {gas_input: "mekanism:osmium"},
+        oxygen: {gas_input: "mekanism:oxygen"},
+        plutonium: {gas_input: "mekanism:plutonium"},
+        polonium: {gas_input: "mekanism:polonium"},
+        sodium: {gas_input: "mekanism:sodium"},
+        spent_nuclear_waste: {gas_input: "mekanism:spent_nuclear_waste"},
+        steam: {gas_input: "mekanism:steam"},
+        sulfur_dioxide: {gas_input: "mekanism:sulfur_dioxide"},
+        sulfur_trioxide: {gas_input: "mekanism:sulfur_trioxide"},
+        sulfuric_acid: {gas_input: "mekanism:sulfuric_acid"},
+        superheated_sodium: {gas_input: "mekanism:superheated_sodium"},
+        uranium_hexafluoride: {gas_input: "mekanism:uranium_hexafluoride"},
+        uranium_oxide: {gas_input: "mekanism:uranium_oxide"},
+        water_vapour: {gas_input: "mekanism:water_vapor"},
+        deuterium: {gas_input: "mekanismgenerators:deuterium"},
+        fusion_fuel: {gas_input: "mekanismgenerators:fusion_fuel"},
+        tritium: {gas_input: "mekanismgenerators:tritium"},
+        neutron_gas: {gas_input: "kubejs:neutron_gas"},
+    }
+
+
+    for (var key in block_dict) {
+        var value = block_dict[key]
+
+        event.custom({
+            type: "mekanism:nucleosynthesizing",
+            duration: value.duration ? value.duration : 400,
+            gasInput: {
+                amount: value.gas_amount ? value.gas_amount : 10000,
+                gas: value.gas_input
+            },
+            itemInput: {
+                ingredient: {
+                    item: value.input ? value.input : "allthecompressed:cobblestone_9x"
+                }
+            },
+            output: {
+                // item: value.output
+                item: "kubejs:block_of_" + key.toString().toLowerCase()
             }
-        },
-        output: {
-            item: "kubejs:block_of_antimatter"
-        }
-    })
+        })
+        
+    }
 
     // creative antimatter chemical tank
     event.custom({
