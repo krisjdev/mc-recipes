e799e3a1dba7f1f2d3010724ba92924a9be1be1c
kris j <kris.jurkans@gmail.com>
2025-05-06 15:11:11 +0100

    add functionality to iterate through dictionary to create blocks

--- kubejs/startup_scripts/kris-additions/creative-tanks-startup.js
+++ kubejs/startup_scripts/kris-additions/creative-tanks-startup.js
@@ -1,13 +1,23 @@
 // https://kubejs.com/wiki/tutorials/block-registry
 // https://kubejs.com/wiki/ref/BlockBuilder
+// https://minecraft.fandom.com/wiki/Formatting_codes?so=search#Use_in_server.properties_and_pack.mcmeta
 
 StartupEvents.registry("block", event => {
-    
-    event.create("block_of_antimatter")
-        .displayName("\u00A7dBlock of Antimatter") // https://minecraft.fandom.com/wiki/Formatting_codes?so=search#Use_in_server.properties_and_pack.mcmeta
-        .stoneSoundType()
-        .hardness(1.0)
-        .resistance(1.0)
-        .tagBlock("krisadditions:block_of_antimatter")
 
+    var block_dict = {
+        // can't use Text.gold() (or other colours) in tooltip
+        antimatter: {tooltip: "\u00A7dBlock of Antimatter"},
+        brine: {tooltip: "\u00A7eBlock of Brine"},
+        chlorine: {tooltip: "\u00A7eBlock of Chlorine"}
+    }
+
+    for (var key in block_dict) {
+        let tag_str = "block_of_" + key.toString().toLowerCase()
+        event.create(tag_str)
+            .displayName(block_dict[key].tooltip)
+            .stoneSoundType()
+            .hardness(1.0)
+            .resistance(1.0)
+            .tagBlock("krisadditions:" + tag_str)
+    }
 })
