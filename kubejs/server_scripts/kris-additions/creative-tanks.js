8de74de9e03cb7fdb210be182d905461aec74d0f
kris j <kris.jurkans@gmail.com>
2025-05-06 19:56:20 +0100

    generate recipes for creative chemical tanks

--- kubejs/server_scripts/kris-additions/creative-tanks.js
+++ kubejs/server_scripts/kris-additions/creative-tanks.js
@@ -65,6 +65,8 @@ ServerEvents.recipes(event => {
     for (var key in block_dict) {
         var value = block_dict[key]
 
+        var block_tag = "kubejs:block_of_" + key.toString().toLowerCase()
+
         event.custom({
             type: "mekanism:nucleosynthesizing",
             duration: value.duration ? value.duration : 400,
@@ -78,39 +80,38 @@ ServerEvents.recipes(event => {
                 }
             },
             output: {
-                // item: value.output
-                item: "kubejs:block_of_" + key.toString().toLowerCase()
+                item: block_tag
             }
         })
-        
+              
+        var nbt = '{mekData:{GasTanks:[{Tank:0b,stored:{amount:9223372036854775806.999,gasName:"' + value.gas_input + '"}}]}}'
+
+        // creative chemical tank variants
+        event.custom({
+            type: "powah:energizing",
+            ingredients: [
+                // TODO: can we use NBT items? doesn't seem like it... :(
+                    /*
+                    fails with:
+                    - Error in 'ServerEvents.recipes': Cannot convert {mekData:{GasTanks:[{Tank:0b,stored:{amount:1,gasName:"mekanism:antimatter"}}]}} to java.lang.Integer
+                    - …rhino.EvaluatorException: Cannot convert {mekData:{GasTanks:[{Tank:0b,stored:{amount:1,gasName:"mekanism:antimatter"}}]}} to java.lang.Integer
+                    
+                    */
+                   // Ingredient.of("mekanism:ultimate_chemical_tank", '{mekData:{GasTanks:[{Tank:0b,stored:{amount:1,gasName:"mekanism:antimatter"}}]}}'),
+                   // Ingredient.of("mekanism:ultimate_chemical_tank", '{mekData:{GasTanks:[{Tank:0b,stored:{amount:"8192000",gasName:"mekanism:antimatter"}}]}}').toJson(),
+
+                   Ingredient.of("mekanism:creative_chemical_tank",),
+                   Ingredient.of("mekanism:creative_chemical_tank",),
+                   Ingredient.of("allthetweaks:atm_star"),
+                   Ingredient.of("mekanism:creative_chemical_tank",),
+                   Ingredient.of("mekanism:creative_chemical_tank",),
+                   Ingredient.of(block_tag),
+                ],
+                energy: "2147483647",
+                // 9223372036854775807 is the wanted value for the amount stored, but that gets translated into a string that breaks the infinite functionality of the tank
+                // 4294967294 is the upper limit, -0.001 to prevent it from casting it into a string
+                // or not... because 9223372036854775807-0.001 works..?
+                result: Item.of("mekanism:creative_chemical_tank", nbt)
+            })      
     }
-
-    // creative antimatter chemical tank
-    event.custom({
-        type: "powah:energizing",
-        ingredients: [
-            // TODO: can we use NBT items? doesn't seem like it... :(
-            /*
-                fails with:
-                - Error in 'ServerEvents.recipes': Cannot convert {mekData:{GasTanks:[{Tank:0b,stored:{amount:1,gasName:"mekanism:antimatter"}}]}} to java.lang.Integer
-                - …rhino.EvaluatorException: Cannot convert {mekData:{GasTanks:[{Tank:0b,stored:{amount:1,gasName:"mekanism:antimatter"}}]}} to java.lang.Integer
-
-            */
-            // Ingredient.of("mekanism:ultimate_chemical_tank", '{mekData:{GasTanks:[{Tank:0b,stored:{amount:1,gasName:"mekanism:antimatter"}}]}}'),
-            // Ingredient.of("mekanism:ultimate_chemical_tank", '{mekData:{GasTanks:[{Tank:0b,stored:{amount:"8192000",gasName:"mekanism:antimatter"}}]}}').toJson(),
-            Ingredient.of("mekanism:creative_chemical_tank",),
-            Ingredient.of("mekanism:creative_chemical_tank",),
-            Ingredient.of("allthetweaks:atm_star"),
-            Ingredient.of("mekanism:creative_chemical_tank",),
-            Ingredient.of("mekanism:creative_chemical_tank",),
-            Ingredient.of("kubejs:block_of_antimatter",),
-        ],
-        energy: "2147483647",
-        // 9223372036854775807 is the wanted value for the amount stored, but that gets translated into a string that breaks the infinite functionality of the tank
-        // 4294967294 is the upper limit, -0.001 to prevent it from casting it into a string
-        // or not... because 9223372036854775807-0.001 works..?
-        // result: Item.of("mekanism:creative_chemical_tank", '{mekData:{GasTanks:[{Tank:0b,stored:{amount:4294967293.9999,gasName:"mekanism:antimatter"}}]}}')
-        result: Item.of("mekanism:creative_chemical_tank", '{mekData:{GasTanks:[{Tank:0b,stored:{amount:9223372036854775806.999,gasName:"mekanism:antimatter"}}]}}')
-      }).id("krisadditions:energizing/mekanism_creative_chemical_tank_antimatter")
-
 })
