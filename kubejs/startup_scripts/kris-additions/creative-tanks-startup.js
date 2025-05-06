56290dd23f13d6d5fbe190661d4dbef55bc03b3c
kris j <kris.jurkans@gmail.com>
2025-05-06 02:45:25 +0100

    add example block of antimatter

---
+++ kubejs/startup_scripts/kris-additions/creative-tanks-startup.js
@@ -0,0 +1,10 @@
+StartupEvents.registry("block", event => {
+    
+    event.create("block_of_antimatter")
+        .displayName("\u00A7dBlock of Antimatter") // https://minecraft.fandom.com/wiki/Formatting_codes?so=search#Use_in_server.properties_and_pack.mcmeta
+        .stoneSoundType()
+        .hardness(1.0)
+        .resistance(1.0)
+        .tagBlock("krisadditions:block_of_antimatter")
+
+})
