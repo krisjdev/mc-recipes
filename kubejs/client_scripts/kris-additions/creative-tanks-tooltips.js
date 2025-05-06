7c70575034c8bef442e2b300fdd6fcc909fc18d0
kris j <kris.jurkans@gmail.com>
2025-05-06 15:10:25 +0100

    replace motd code with function call

--- kubejs/client_scripts/kris-additions/creative-tanks-tooltips.js
+++ kubejs/client_scripts/kris-additions/creative-tanks-tooltips.js
@@ -2,5 +2,5 @@
 // https://minecraft.fandom.com/wiki/Formatting_codes?so=search#Use_in_server.properties_and_pack.mcmeta
 
 ItemEvents.tooltip(event => {
-    event.add("kubejs:block_of_antimatter", ["\u00A76Kris' Additions"])
+    event.add("kubejs:block_of_antimatter", [Text.gold("Kris' Additions - Creative Tanks").italic()])
 })
